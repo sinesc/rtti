ea743a84468b7f6423426b755279aff0fcac606a
Dennis Möhlmann <dmo479@gmail.com>
2018-02-20 07:37:56 +0100

    determine offsets

--- src/lib.rs
+++ src/lib.rs
@@ -16,14 +16,16 @@ macro_rules! implement_rtti {
     )
 }
 
-implement_rtti!(i8, i8);
-implement_rtti!(i16, i16);
-implement_rtti!(i32, i32);
-implement_rtti!(i64, i64);
+implement_rtti!(usize, usize);
+implement_rtti!(isize, isize);
 implement_rtti!(u8, u8);
+implement_rtti!(i8, i8);
 implement_rtti!(u16, u16);
+implement_rtti!(i16, i16);
 implement_rtti!(u32, u32);
+implement_rtti!(i32, i32);
 implement_rtti!(u64, u64);
+implement_rtti!(i64, i64);
 implement_rtti!(f32, f32);
 implement_rtti!(f64, f64);
 implement_rtti!(char, char);
@@ -32,7 +34,9 @@ implement_rtti!(bool, bool);
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
 pub enum Type {
-    NoData,
+    Ignored,
+    usize,
+    isize,
     u8,
     i8,
     u16,
@@ -59,8 +63,8 @@ pub enum Visibility {
 
 #[derive(Debug)]
 pub struct Field {
-    pub name: String,
     pub vis: Visibility,
+    pub offset: usize,
     pub ty: Box<Type>,
 }
 
@@ -68,5 +72,5 @@ pub struct Field {
 pub struct Struct {
     pub name: String,
     pub vis: Visibility,
-    pub fields: Vec<Field>,
+    pub fields: Vec<(String, Field)>,
 }
