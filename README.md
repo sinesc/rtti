4186956ecebd26c09b68ac97588af61f6804858b
Dennis Möhlmann <dmo479@gmail.com>
2018-02-26 08:58:03 +0100

    include readme, update docs

---
+++ README.md
@@ -0,0 +1,7 @@
+# RTTI-derive
+
+Procedural macro to derive RTTI trait. See crate [`rtti`](https://crates.io/crates/rtti).
+
+**very early, probably best to stay away for now**
+
+[Documentation](https://docs.rs/rtti/).
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 /*!
-
-Run-time type information trait. Use crate rtti-derive to implement.
-
-**very early, probably best to stay away for now**
+Run-time type information trait. Use crate [`rtti-derive`](https://crates.io/crates/rtti-derive) to implement.
 
 To include RTTI, use:
 
